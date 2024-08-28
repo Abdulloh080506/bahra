ebd7bac9f465cfdb380c0f82c7aa5a6556718317
Abdulloh080506 <tohirov080506@gmail.com>
2024-08-28 13:26:34 +0500

    first commit

---
+++ js/template.js
@@ -0,0 +1,8 @@
+const selectElement = (element, parentElement = document) => parentElement.querySelector(element);
+const elProducts = selectElement('#product_template1')
+
+function renderTemplate(templateArr, element){
+
+}
+
+renderTemplate(products, )
---
+++ js/utils.js
@@ -0,0 +1,11 @@
+const elTemplate1 = selectElement('#template');
+const elTemplate2 = selectElement('#template2');
+
+const products = [
+    {
+        elTemplate1
+    },
+    {
+        elTemplate2
+    }
+] 
